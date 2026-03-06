16201b78341516d14a6beec1b5fe36f0eccf17ae
Renato Takao Matura de Lima <renato.matura@gmail.com>
2026-03-06 10:06:42 -0300

    Tailwind css file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client/react";
 import { apolloClient } from "./lib/graphql/apollo.ts";
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
